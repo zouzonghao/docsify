2c296e6861bdf4ea0ccb8e8fe07bbba57e2db239
sanqiz37 <1254751767@qq.com>
2023-02-21 21:18:04 +0800

    modified 2023/02/21 -21:18:04

--- push.sh
+++ push.sh
@@ -5,6 +5,6 @@ current_time=$(date "+%Y/%m/%d -%H:%M:%S") # 获取当前时间
 echo ${current_time} # 显示当前时间
 git add .
 git commit -m "modified ${current_time}" # 远程仓库可以看到是什么时间修改的...
-git push origin main
+git push 
 echo "####### 自动Git完成 #######"
 sleep 2s 
